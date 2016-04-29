376a485e5aa812e789270d418242b8d5e0d260e5
Lioncash <mathew1800@gmail.com>
2016-04-29 11:23:40 -0400

    vertex_loader: Add constructors to facilitate immediate and two-step initialization

--- src/video_core/vertex_loader.h
+++ src/video_core/vertex_loader.h
@@ -12,6 +12,11 @@ namespace Pica {
 
 class VertexLoader {
 public:
+    VertexLoader() = default;
+    explicit VertexLoader(const Pica::Regs& regs) {
+        Setup(regs);
+    }
+
     void Setup(const Pica::Regs& regs);
     void LoadVertex(u32 base_address, int index, int vertex, Shader::InputVertex& input, DebugUtils::MemoryAccessTracker& memory_accesses);
 
