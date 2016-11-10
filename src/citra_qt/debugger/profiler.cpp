3015df615f8306c08f5341ca44076a4ef4092d23
emmaus <emmausssss@gmail.com>
2016-11-10 17:51:14 +0000

    initial
    initial
    
    initial
    
    initial
    
    split fps
    
    fix
    
    fps in column
    
    rebased

--- src/citra_qt/debugger/profiler.cpp
+++ src/citra_qt/debugger/profiler.cpp
@@ -26,6 +26,15 @@ static QVariant GetDataForColumn(int col, const AggregatedDuration& duration) {
         return std::chrono::duration_cast<FloatMs>(dur).count();
     };
 
+    static auto duration_to_fps = [](Duration dur) -> float {
+        using FloatMs = std::chrono::duration<float, std::chrono::milliseconds::period>;
+        float fps = 1000/std::chrono::duration_cast<FloatMs>(dur).count();
+        if (dur.count() == 0) {
+            fps = 0;
+        }
+        return fps;
+    };
+
     switch (col) {
     case 1:
         return duration_to_float(duration.avg);
@@ -33,6 +42,8 @@ static QVariant GetDataForColumn(int col, const AggregatedDuration& duration) {
         return duration_to_float(duration.min);
     case 3:
         return duration_to_float(duration.max);
+    case 4:
+        return duration_to_fps(duration.max);
     default:
         return QVariant();
     }
@@ -53,6 +64,8 @@ QVariant ProfilerModel::headerData(int section, Qt::Orientation orientation, int
             return tr("Min");
         case 3:
             return tr("Max");
+        case 4:
+            return tr("Fps(avg)");
         }
     }
 
@@ -68,7 +81,7 @@ QModelIndex ProfilerModel::parent(const QModelIndex& child) const {
 }
 
 int ProfilerModel::columnCount(const QModelIndex& parent) const {
-    return 4;
+    return 5;
 }
 
 int ProfilerModel::rowCount(const QModelIndex& parent) const {
@@ -84,13 +97,16 @@ QVariant ProfilerModel::data(const QModelIndex& index, int role) const {
         if (index.row() == 0) {
             if (index.column() == 0) {
                 return tr("Frame");
-            } else {
+            }
+            else {
                 return GetDataForColumn(index.column(), results.frame_time);
             }
-        } else if (index.row() == 1) {
+        }
+        else if (index.row() == 1) {
             if (index.column() == 0) {
                 return tr("Frame (with swapping)");
-            } else {
+            }
+            else {
                 return GetDataForColumn(index.column(), results.interframe_time);
             }
         }
